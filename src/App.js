c6a418870b06792a5294205cacdc74e2e3f07419
pratham-chawdhry <122348596+pratham-chawdhry@users.noreply.github.com>
2024-04-06 20:09:43 +0530

    fix package.json Attempt-1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieDetail from './Components/Movie/MovieDetailOne';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename = "/">
       <Routes>
         <Route exact path = "/" element = {<HomePage />}/>
         <Route path = "/search" element = {<MovieCentral/>}/>
